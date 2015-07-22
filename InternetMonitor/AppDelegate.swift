77c899889a73a1a4d0054b406de357b3491d8fa6
Christian Beer <christian.beer@chbeer.de>
2015-07-22 12:16:44 +0200

    Initial Commit

---
+++ InternetMonitor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  InternetMonitor
+//
+//  Created by Christian Beer on 22.07.15.
+//  Copyright (c) 2015 Christian Beer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ InternetMonitor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  InternetMonitor
+//
+//  Created by Christian Beer on 22.07.15.
+//  Copyright (c) 2015 Christian Beer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
